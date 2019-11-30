fefeb14a160a9f7064041cefa8c373397cf706f4
Ido Ben-Hur <idoybh2@gmail.com>
2019-12-01 02:27:03 +0700

    AncientSettings: Allow customizing footer text [2/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/com/ancient/settings/fragments/QuickSettings.java
+++ src/com/ancient/settings/fragments/QuickSettings.java
@@ -17,8 +17,12 @@ package com.ancient.settings.fragments;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
@@ -29,20 +33,61 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.ancient.settings.preferences.CustomSeekBarPreference;
+import com.ancient.settings.preferences.SystemSettingEditTextPreference;
 
 public class QuickSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "QuickSettings";
 
+    private static final String CRAFT_FOOTER_TEXT_STRING = "craft_footer_text_string";
+
+    private SystemSettingEditTextPreference mFooterString;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.ancient_settings_quicksettings);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mFooterString = (SystemSettingEditTextPreference) findPreference(CRAFT_FOOTER_TEXT_STRING);
+        mFooterString.setOnPreferenceChangeListener(this);
+        String footerString = Settings.System.getString(getContentResolver(),
+                CRAFT_FOOTER_TEXT_STRING);
+        if (footerString != null && footerString != "")
+            mFooterString.setText(footerString);
+        else {
+            mFooterString.setText("CraftWithHeart");
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.CRAFT_FOOTER_TEXT_STRING, "CraftWithHeart");
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+    	ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFooterString) {
+            String value = (String) newValue;
+            if (value != "" && value != null)
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.CRAFT_FOOTER_TEXT_STRING, value);
+            else {
+                mFooterString.setText("CraftWithHeart");
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.CRAFT_FOOTER_TEXT_STRING, "CraftWithHeart");
+            }
+            return true;
+        }
         return false;
     }
 
