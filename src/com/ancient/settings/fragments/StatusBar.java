25fe3a26c39c73f7519af4cf2653d96ce0339310
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-06-14 21:32:44 +0700

    AncientSettings: Simple Dual statusbar row [2/2]
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/com/ancient/settings/fragments/StatusBar.java
+++ src/com/ancient/settings/fragments/StatusBar.java
@@ -25,19 +25,31 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import androidx.preference.*;
 
+import com.android.internal.util.ancient.Utils;
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.ancient.settings.preferences.SystemSettingSwitchPreference;
+
 public class StatusBar extends SettingsPreferenceFragment implements
     Preference.OnPreferenceChangeListener {
+
+    private static final String STATUSBAR_DUAL_ROW = "statusbar_dual_row";
+
+    private SystemSettingSwitchPreference mStatusbarDualRow;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.ancient_settings_statusbar);
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mStatusbarDualRow = (SystemSettingSwitchPreference) findPreference(STATUSBAR_DUAL_ROW);
+        mStatusbarDualRow.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUSBAR_DUAL_ROW, 0) == 1));
+        mStatusbarDualRow.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -48,5 +60,17 @@ public class StatusBar extends SettingsPreferenceFragment implements
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.ANCIENT_SETTINGS;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mStatusbarDualRow) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_DUAL_ROW, value ? 1 : 0);
+            Utils.showSystemUiRestartDialog(getContext());
+            return true;
+        }
+        return false;
     }
 }
