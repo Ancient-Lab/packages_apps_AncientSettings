39e6d0f9a2568f3fcd534fa38acb5e176360cf07
Idc <59904122+IDontCare-05@users.noreply.github.com>
2020-07-13 11:02:56 +0700

    analog

--- src/com/ancient/settings/fragments/Interface.java
+++ src/com/ancient/settings/fragments/Interface.java
@@ -29,6 +29,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.ancient.settings.fragments.QsTileStylePreferenceController;
 import com.ancient.settings.fragments.QsClockStylePreferenceController;
 import com.ancient.settings.fragments.SettingsStylePreferenceController;
+import com.ancient.settings.fragments.AnalogStylePreferenceController;
 
 import com.android.settings.R;
 
@@ -72,6 +73,7 @@ public class Interface extends DashboardFragment {
         controllers.add(new QsTileStylePreferenceController(context));
         controllers.add(new QsClockStylePreferenceController(context));
         controllers.add(new SettingsStylePreferenceController(context));
+        controllers.add(new AnalogStylePreferenceController(context));
         return controllers;
     }
 }
